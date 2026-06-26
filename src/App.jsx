923fe46985b329771eb4282cbf19eeabf18c903c
Benjamin-66Tar <bemlopez200613@gmail.com>
2026-06-25 19:17:48 -0600

    refactor: configurar Vite, React, .gitignore y renombrar a jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './views/Dashboard';
+import './assets/estilos/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
